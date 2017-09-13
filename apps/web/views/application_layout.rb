b3794e6e8d75742bd1cb1ada86a560e40851942d
greenfork <katar38@gmail.com>
2017-09-13 18:52:58 +0300

    web/home/index finished

--- apps/web/views/application_layout.rb
+++ apps/web/views/application_layout.rb
@@ -2,6 +2,10 @@ module Web
   module Views
     class ApplicationLayout
       include Web::Layout
+
+      def title
+        t "default.title"
+      end
     end
   end
 end
--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -1,5 +1,13 @@
 module Web::Views::Home
   class Index
     include Web::View
+
+    def title
+      t ".title"
+    end
+
+    def active_quizzes
+      QuizRepository.new.active_quizzes
+    end
   end
 end
