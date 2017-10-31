f7134663e0b933bc90a0ceda02ce8ad0b78f2d31
Dmitry Matveyev <dm.matveyev@gmail.com>
2017-10-31 17:23:50 +0300

    Add i18n support

--- spec/research/controllers/analysis/dictionary_spec.rb
+++ spec/research/controllers/analysis/dictionary_spec.rb
@@ -61,12 +61,6 @@ describe Research::Controllers::Analysis::Dictionary do
         response[0].must_equal 200
         action.brief.keys.must_include :total
       end
-
-      it 'stops if word is absent in the database' do
-        params[:selection][:word] = 'not_existant_word'
-        response = action.call(params)
-        response[0].must_equal 422
-      end
     end
 
     describe 'with full list of parameters' do
