e28a808992541ee746ac09d15c54a7e1d38100b4
Dmitry Matveyev <dm.matveyev@gmail.com>
2017-11-01 09:26:40 +0300

    Add i18n for errors

--- apps/research/views/analysis/dictionary.rb
+++ apps/research/views/analysis/dictionary.rb
@@ -6,7 +6,14 @@ module Research::Views::Analysis
       form_for :selection, routes.dict_path, class: 'form-horizontal' do
         # word
         div(class: 'form-group') do
-          div(show_error("word #{params[:selection][:word]} not in database")) if params.errors.dig(:word)
+          if params.errors.dig(:word)
+            div(
+              show_error(
+                t('.errors.word_not_in_database',
+                  word: params[:selection][:word])
+              )
+            )
+          end
           label t('.word'), for: 'selection-word', class: 'col-sm-2 control-label'
           div(class: 'col-sm-10') do
             text_field :word, class: 'form-control'
@@ -54,7 +61,7 @@ module Research::Views::Analysis
         # age
         div(class: 'form-group') do
           if params.errors.dig(:selection, :age_from) || params.errors.dig(:selection, :age_to)
-            div(show_error("age must be int and 1..100"))
+            div(show_error(t('.errors.irregular_age')))
           end
           label t('.age'), for: 'selection-age-from', class: 'col-sm-2 control-label'
           div(class: 'col-sm-2') do
@@ -95,7 +102,7 @@ module Research::Views::Analysis
         # date
         div(class: 'form-group') do
           if params.errors.dig(:selection, :date_from) || params.errors.dig(:selection, :date_to)
-            div(show_error('bad date format'))
+            div(show_error(t('.errors.bad_date_format')))
           end
           label t('.date'), for: 'selection-date-from', class: 'col-sm-2 control-label'
           div(class: 'col-sm-2') do
