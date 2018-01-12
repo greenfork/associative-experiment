2d9f0b0f9aa65b01a508ee31ebe0d25c3e906a00
greenfork <dm.matveyev@protonmail.com>
2018-01-12 04:30:19 +0300

    Refactor analysis#Stats::Dictionary in stats_dictionary.rb

--- apps/research/controllers/analysis/dictionary/stats_dictionary.rb
+++ apps/research/controllers/analysis/dictionary/stats_dictionary.rb
@@ -5,6 +5,7 @@ module Research::Controllers::Analysis::Stats
     def initialize(reactions)
       @reactions = reactions
       @dictionary = []
+      @reaction_list = []
       generate_dictionary
     end
 
@@ -22,36 +23,21 @@ module Research::Controllers::Analysis::Stats
     # hash = { reaction: 'reac1', count: 18, translation: 'tr1',
     #  translation_comment: 'tr_c1'}
     def insert_hashes
-      reaction_list.each do |reaction|
+      reactions.to_a.each do |reac|
+        reaction = reac.reaction
+        next if @reaction_list.include? reaction
+        @reaction_list.push reaction
+        count = count_occurences(reaction)
         hash = { reaction: reaction,
-                 count: count_occurences(reaction),
-                 translation: find_translation(reaction)[:translation],
-                 translation_comment: find_translation(reaction)[:comment] }
+                 count: count,
+                 translation: reac.translation,
+                 translation_comment: reac.translation_comment }
         dictionary << hash
       end
     end
 
     def count_occurences(reaction)
-      reaction_array.count(reaction)
-    end
-
-    def reaction_array
-      reactions.to_a.map do |r|
-        r.reaction.nil? ? 'nil' : r.reaction
-      end
-    end
-
-    def reaction_list
-      reaction_array.uniq
-    end
-
-    def find_translation(reaction)
-      reactions.to_a.map do |r|
-        if r.reaction == reaction
-          return { translation: r.translation, comment: r.translation_comment }
-        end
-      end
-      { translation: nil, comment: nil }
+      reactions.to_a.count { |r| r.reaction == reaction }
     end
   end
 end
