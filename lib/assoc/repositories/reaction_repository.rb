0428321361e67bf08d9e5b7387b09b0ab8c9d12b
greenfork <dm.matveyev@protonmail.com>
2018-01-14 00:55:07 +0300

    Add reaction repository function to process dictionary in database

--- lib/assoc/repositories/reaction_repository.rb
+++ lib/assoc/repositories/reaction_repository.rb
@@ -38,4 +38,32 @@ class ReactionRepository < Hanami::Repository
     list = list.map { |r| r.merge(created_at: time, updated_at: time) }
     command(:create, reactions, result: :many).call(list)
   end
+
+  def get_dictionary(quiz_id: nil,
+                     options: { reactions: {}, people: {} },
+                     type: :straight)
+    # TODO: add sex, age, native_language, date where clauses
+    reaction = options[:reactions][:reaction]
+    nationality1 = options[:people][:nationality1]
+    unless reaction.nil?
+      w_reaction = " AND reaction = '#{reaction.gsub("'", "''")}'"
+    end
+    unless nationality1.nil?
+      w_nationality1 = " AND nationality1 = '#{nationality1.gsub("'", "''")}'"
+    end
+    w_quiz_id = " AND r.quiz_id = #{Integer(quiz_id)}" unless quiz_id.nil?
+    order = 'stimulus, pair_count DESC, reaction' if type == :straight
+    order = 'reaction, pair_count DESC, stimulus' if type == :reversed
+
+    sql = <<-SQL.gsub(/^ */, '')
+        SELECT r.reaction, r.translation, s.stimulus, COUNT(*) as pair_count
+        FROM reactions r
+        JOIN stimuli s ON s.id = r.stimulus_id
+        JOIN people p ON p.id = r.person_id
+        WHERE 1 = 1#{w_quiz_id}#{w_reaction}#{w_nationality1}
+        GROUP BY reaction, stimulus
+        ORDER BY #{order}
+    SQL
+    root.read(sql).to_a
+  end
 end
