fd46549a1c65421900954464acd5293923a507f1
Dmitry Matveyev <dm.matveyev@protonmail.com>
2018-05-03 20:45:36 +0300

    Move LocaleHelper module from application.rb to external file

--- apps/research/application.rb
+++ apps/research/application.rb
@@ -1,23 +1,6 @@
 require 'hanami/helpers'
 require 'hanami/assets'
-
-module LocaleHelper
-  def t(key, options = {})
-    if key.to_s[0] == '.'
-      app, _, controller, action = self.class.name.split('::').map(&:downcase)
-      scope    = "#{app}.#{controller}"
-      defaults = [:"#{scope}#{key}"]
-      scope    << ".#{action}"
-      defaults << :"#{scope}#{key}"
-      defaults << options[:default] if options[:default]
-
-      options[:default] = defaults
-      key = "#{scope}#{key}"
-    end
-
-    ::I18n.t key, options
-  end
-end
+require_relative 'locale_helper.rb'
 
 module Research
   class Application < Hanami::Application
