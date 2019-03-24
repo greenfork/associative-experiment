cac00be05159444616a7d18f7ad5abf36f7ca3c2
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-03-24 19:00:56 +0300

    Add description to forms#upload

--- apps/admin/views/forms/upload.rb
+++ apps/admin/views/forms/upload.rb
@@ -17,25 +17,94 @@ module Admin
             end
 
             div class: 'form-group' do
-              label t('.respondents'), for: 'form-respondents',
+              label t('.respondents.label'), for: 'form-respondents',
                     class: 'col-sm-2 control-label'
               div class: 'col-sm-10' do
                 file_field :respondents
               end
             end
 
+            div class: 'col-sm-10 col-sm-offset-2' do
+              respondents_file_description
+            end
+
+
+
             div class: 'form-group' do
-              label t('.forms'), for: 'form-respondents',
+              label t('.forms.label'), for: 'form-forms',
                     class: 'col-sm-2 control-label'
               div class: 'col-sm-10' do
                 file_field :forms
               end
             end
+            div class: 'col-sm-10 col-sm-offset-2' do
+              forms_file_description
+            end
 
             div class: 'form-group' do
               div(class: 'col-sm-offset-2 col-sm-10') do
-                submit t('.check'), id: 'submit', class: 'btn btn-default'
+                submit t('.check'), id: 'submit', class: 'btn btn-primary'
               end
+            end
+          end
+        end
+
+        def general_file_requirements
+          html.div do
+            p t('.file_requirements.title') << ':'
+            ul do
+              li t('.file_requirements.point1')
+              li t('.file_requirements.point2')
+            end
+          end
+        end
+
+        def respondents_file_description
+          html.div do
+            p t('.respondents.title') << ':'
+            p t('.respondents.description')
+            p do
+              button t('.respondents.attributes_button'),
+                     class: 'btn btn-default',
+                     'data-toggle': 'collapse',
+                     'data-target': '#respondents-attributes',
+                     type: 'button'
+            end
+            ul id: 'respondents-attributes', class: 'collapse' do
+              li t('.respondents.id')
+              li t('.respondents.sex')
+              li t('.respondents.age')
+              li t('.respondents.profession')
+              li t('.respondents.region')
+              li t('.respondents.residence_place')
+              li t('.respondents.birth_place')
+              li t('.respondents.nationality1')
+              li t('.respondents.nationality2')
+              li t('.respondents.spoken_languages')
+              li t('.respondents.native_language')
+              li t('.respondents.communication_language')
+              li t('.respondents.education_language')
+            end
+          end
+        end
+
+        def forms_file_description
+          html.div do
+            p t('.forms.title') << ':'
+            p t('.forms.description')
+            p do
+              button t('.forms.attributes_button'),
+                     class: 'btn btn-default',
+                     'data-toggle': 'collapse',
+                     'data-target': '#forms-attributes',
+                     type: 'button'
+            end
+            ul id: 'forms-attributes', class: 'collapse' do
+              li t('.forms.stimulus')
+              li t('.forms.stimulus_translation')
+              li t('.forms.reaction')
+              li t('.forms.reaction_translation')
+              li t('.forms.reaction_translation_comment')
             end
           end
         end
