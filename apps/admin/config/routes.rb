e463ec597707b354599c69c536b81b04e08d0aef
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-04-10 09:31:27 +0300

    Implement parsing

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -5,5 +5,6 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 root to: 'forms#upload'
 get '/forms/upload', to: 'forms#upload', as: 'upload'
+post '/forms/upload', to: 'forms#upload'
 get '/login', to: 'authentication#login', as: 'auth'
 post '/login', to: 'authentication#login'
--- apps/admin/controllers/forms/upload.rb
+++ apps/admin/controllers/forms/upload.rb
@@ -5,6 +5,21 @@ module Admin
         include Admin::Action
 
         def call(params)
+          if request.post?
+            parser = ParseFormsFromExcel.new(
+              respondents_xlsx: params[:form][:respondents][:tempfile],
+              respondents_headers: %w[id sex age profession region
+                                      residence_place birth_place
+                                      nationality1 nationality2
+                                      spoken_languages native_language
+                                      communication_language
+                                      education_language],
+              forms_xlsx: params[:form][:forms][:tempfile],
+              forms_headers: %w[person_id stimulus stimulus_translation
+                                reaction reaction_translation
+                                reaction_translation_comment]
+            ).call
+          end
         end
       end
     end
