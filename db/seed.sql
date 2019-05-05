c93514c038dc59174ea51b38b7265348738d9105
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-05-05 21:30:30 +0300

    Add seed.sql file for adding admin user with admin:admin credentials

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (login, hashed_pass, email, created_at, updated_at) VALUES
+('admin', '$2a$10$wLvAMGRZSlcucZ.9oA1Yfe1Fuun0UqkkQyUp4ElUqn3xgVCzu.neS',
+ 'admin@assoc.com', NOW(), NOW());
