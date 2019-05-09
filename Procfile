ac51523764358c8c92b2b40f2f4b3f4c49c2eb57
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-05-09 23:35:42 +0300

    Set up unicorn server

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec unicorn -c config/unicorn.rb -E production
+release: bundle exec hanami db migrate
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,13 +3,13 @@ shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 worker_processes ENV['UNICORN_WORKER_PROCESSES'].to_i
-preload_app true
+# preload_app true
 timeout ENV['UNICORN_TIMEOUT'].to_i
 
-listen "#{shared_dir}/sockets/unicorn.sock", backlog: 64
+# listen "#{shared_dir}/sockets/unicorn.sock", backlog: 64
 # listen "#{ENV['UNICORN_HOST']}:#{ENV['UNICORN_PORT']}", tcp_nopush: true
 
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+# stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+# stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
-pid "#{shared_dir}/pids/unicorn.pid"
+# pid "#{shared_dir}/pids/unicorn.pid"
